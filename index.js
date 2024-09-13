c66f9a02bb34358237f8a27404f1caf42fdf3073
Jefferson <jeffersonformigoni86@gmail.com>
2024-09-13 11:40:16 -0300

    fundamentos 005

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-// function start() {
-//     console.log('começou');
-// }
-// start();
-
 const start = () => {
-    let count = 0;
-    while (count < 10) {
-        console.log(count);
-        count = count + 1;
+
+    while(true){
+        let opcao = "sair"
+        switch(opcao) {
+            case "cadastrar":
+                console.log('Vamos cadastrar')
+                break
+            case "listar":
+                console.log('Vamos listar')
+                break
+            case "sair":
+                return            
+        }
     }
 }
 
-start();
+start()
